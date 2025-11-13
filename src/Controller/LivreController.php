6f66668ce9acf5cbece51515c814be84460645d3
Florent Mineiro <florent.mineiro.93@gmail.com>
2025-11-13 07:32:59 +0100

    description du livre

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -70,6 +70,73 @@ données.',
          
              
    }
-  
+
+   #[Route('/livre/{id}')]
+   public function livre(int $id) : Response
+   {
+      $livres = [
+ 1 => [
+ 'id' => 1,
+ 'titre' => 'Introduction aux Algorithmes',
+ 'auteur' => 'Thomas H. Cormen',
+ 'isbn' => '978-2100545261',
+ 'genre' => 'informatique',
+ 'annee_publication' => 2010,
+ 'nombre_pages' => 1200,
+ 'disponible' => true,
+ 'nombre_exemplaires' => 3,
+ 'resume' => 'Manuel de référence couvrant les algorithmes fondamentaux et les structures de 
+données.',
+ 'editeur' => 'Dunod',
+ 'cote' => 'INF.004.COR'
+ ],
+ 2 => [
+ 'id' => 2,
+ 'titre' => 'Le Rouge et le Noir',
+ 'auteur' => 'Stendhal',
+ 'isbn' => '978-2070360024',
+ 'genre' => 'litterature',
+ 'annee_publication' => 1830,
+ 'nombre_pages' => 720,
+ 'disponible' => false,
+ 'nombre_exemplaires' => 0,
+ 'resume' => 'Roman emblématique du XIXe siècle suivant les ambitions de Julien Sorel.',
+ 'editeur' => 'Gallimard',
+'cote' => 'LIT.840.STE'
+ ],
+ 3 => [
+ 'id' => 3,
+ 'titre' => 'Physique Quantique - Fondements et Applications',
+ 'auteur' => 'Michel Le Bellac',
+ 'isbn' => '978-2759807802',
+ 'genre' => 'sciences',
+ 'annee_publication' => 2013,
+ 'nombre_pages' => 450,
+ 'disponible' => true,
+ 'nombre_exemplaires' => 2,
+ 'resume' => 'Introduction moderne à la mécanique quantique avec applications pratiques.',
+ 'editeur' => 'EDP Sciences',
+ 'cote' => 'PHY.530.LEB'
+   ]
+ ];
+      $livre = $livres[$id] ?? null;
+ 
+          return $this->render('description.html.twig', [
+               
+                  'livre' => $livre,
+                      
+         
+        ]);
+        
+        if (!livres[$id])
+        {
+          throw $this->createNotFoundException("Ce livre n'existe pas ");
+        }
+           
+
+        
+   }
+   
+   
   
 }
