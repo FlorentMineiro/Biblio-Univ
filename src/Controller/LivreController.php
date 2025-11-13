27ee96c4125c6a60d74a0b203e2ecb5ba31797fd
Florent Mineiro <florent.mineiro.93@gmail.com>
2025-11-13 10:14:58 +0100

    suppression du duplicate code

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -10,12 +10,9 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LivreController extends AbstractController
 {
-  
-   #[Route('/Catalogue')]
-   public function app_catalogue_liste() : Response
-   {
-$livres = [
- 1 => [
+  private array $livres = 
+  [
+    1 => [
  'id' => 1,
  'titre' => 'Introduction aux Algorithmes',
  'auteur' => 'Thomas H. Cormen',
@@ -58,12 +55,18 @@ données.',
  'editeur' => 'EDP Sciences',
  'cote' => 'PHY.530.LEB'
    ]
+ 
  ];
+  
+   #[Route('/Catalogue')]
+   public function app_catalogue_liste() : Response
+   {
+
 
          
               return $this->render('Catalogue/catalogue.html.twig', [
                
-                  'livres' => $livres,
+                  'livres' => $this->livres,
                       
          
         ]);
@@ -74,51 +77,7 @@ données.',
    #[Route('/livre/{id}')]
    public function livre(int $id) : Response
    {
-      $livres = [
- 1 => [
- 'id' => 1,
- 'titre' => 'Introduction aux Algorithmes',
- 'auteur' => 'Thomas H. Cormen',
- 'isbn' => '978-2100545261',
- 'genre' => 'informatique',
- 'annee_publication' => 2010,
- 'nombre_pages' => 1200,
- 'disponible' => true,
- 'nombre_exemplaires' => 3,
- 'resume' => 'Manuel de référence couvrant les algorithmes fondamentaux et les structures de 
-données.',
- 'editeur' => 'Dunod',
- 'cote' => 'INF.004.COR'
- ],
- 2 => [
- 'id' => 2,
- 'titre' => 'Le Rouge et le Noir',
- 'auteur' => 'Stendhal',
- 'isbn' => '978-2070360024',
- 'genre' => 'litterature',
- 'annee_publication' => 1830,
- 'nombre_pages' => 720,
- 'disponible' => false,
- 'nombre_exemplaires' => 0,
- 'resume' => 'Roman emblématique du XIXe siècle suivant les ambitions de Julien Sorel.',
- 'editeur' => 'Gallimard',
-'cote' => 'LIT.840.STE'
- ],
- 3 => [
- 'id' => 3,
- 'titre' => 'Physique Quantique - Fondements et Applications',
- 'auteur' => 'Michel Le Bellac',
- 'isbn' => '978-2759807802',
- 'genre' => 'sciences',
- 'annee_publication' => 2013,
- 'nombre_pages' => 450,
- 'disponible' => true,
- 'nombre_exemplaires' => 2,
- 'resume' => 'Introduction moderne à la mécanique quantique avec applications pratiques.',
- 'editeur' => 'EDP Sciences',
- 'cote' => 'PHY.530.LEB'
-   ]
- ];
+    $livres;
       $livre = $livres[$id] ?? null;
  
           return $this->render('description.html.twig', [
@@ -130,12 +89,23 @@ données.',
         
         if (!livres[$id])
         {
-          throw $this->createNotFoundException("Ce livre n'existe pas ");
+          return $this->createNotFoundException("Ce livre n'existe pas ");
         }
            
 
         
    }
+   #[Route('/catalogue/genre/{genre}')]
+   public function genre(int $id) : Response
+   {
+     
+   return $this->render('Catalogue/genre/genre.html.twig',[
+      'genreLivre'=> $genreLivre[$id],
+      'livre'=> $livres[$id],
+ ]);
+
+   }
+  
    
    
   
